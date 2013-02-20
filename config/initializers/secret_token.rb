d1d663d138b5af7023065c5dc13cf1a35bb323c2
PjpG <patryk@doppio.jp>
2013-02-20 19:15:46 +0100

    Make default tree.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adpa::Application.config.secret_token = '6a34ede0f062535809b05f22f57f37a03a7d8db44f2615d67c626b5d3246b82f8fbf3a8e4de6b71e402c59a0ea8a42713a06b42894ef2bc353bf4cd7854f9ba9'
